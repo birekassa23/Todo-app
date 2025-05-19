81308f7d45a1ffff3149be0f1ef3bb29ecef3685
birekassa23 <birekassa17828062@gmail.com>
2025-05-19 07:49:44 +0300

    added to do item

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+// src/components/TodoItem.js
+import React from "react";
+
+function TodoItem({ todo, deleteTodo, toggleTodo }) {
+  return (
+    <li style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+      <span onClick={() => toggleTodo(todo.id)}>{todo.text}</span>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </li>
+  );
+}
+
+export default TodoItem;
